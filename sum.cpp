c1e5114784941a956b0017cf0ee5b504d5f8b47d
julencasazk <julencasazk@gmail.com>
2020-01-17 16:56:32 +0100

    none

--- sum.cpp
+++ sum.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+/*
+ * Simple main function of this program:
+ * To ask, read and sum two numbers from user input and output the result
+ *
+ */
 
 int main()
 {
     std::cout << "Escribe dos numeros enteros: " << std::endl;
     int num1 = 0, num2; //No need to give a value (no need to initialize)
-    std::cin >> num1 >> num2;
+    std::cin >> num1 >> num2; // Reading the input from the user
     std::cout << "La suma de " << num1 << " y " << num2 << " es: " << num1 + num2 << std::endl;
-    std::cout << "Bruh" << std::endl;
-    int v1 = 0;
-    std::cin >> v1;
     return 0;
 }
 
