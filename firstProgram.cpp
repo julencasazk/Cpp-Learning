ca01232b5900afb47070aaa8bb0252c160c5c6fb
julencasazk <julencasazk@gmail.com>
2020-01-17 16:40:24 +0100

    none

---
+++ firstProgram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Escribe dos numeros enteros: " << std::endl;
+    int num1 = 0, num2; //No need to give a value (no need to initialize)
+    std::cin >> num1 >> num2;
+    std::cout << "La suma de " << num1 << " y " << num2 << " es: " << num1 + num2 << std::endl;
+    std::cout << "Bruh" << std::endl;
+    int v1 = 0;
+    std::cin >> v1;
+    return 0;
+}
+
