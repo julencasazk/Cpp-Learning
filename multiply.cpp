879b7d0680dcc52660a6a0af2ab185e1372e2cc9
julencasazk <julencasazk@gmail.com>
2020-01-17 16:48:29 +0100

    none

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input 2 numbers: " << std::endl;
+    int var1 = 0, var2 = 0;
+    std::cin >> var1 >> var2;
+    std::cout << "The multiplication of your numbers ( " << var1 << " * " << var2 << " ) is: " << var1 * var2 << std::endl;
+    return 0;
+     
+}
