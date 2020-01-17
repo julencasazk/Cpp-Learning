4ef005c824b57b03f67103ab395041d9ab39bc91
julencasazk <julencasazk@gmail.com>
2020-01-17 17:25:51 +0100

    none

--- while.cpp
+++ while.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-    int suma = 0,  val = 1;
-    while (val <= 10) {
+    int suma = 0,  val = 50;
+    while (val <= 100) {
         suma += val; // Asign suma as suma + val
         ++val; //  Adds 1 to val
     }
-    std::cout << "The sum of all ints from 1 to 10 inclusive is " << suma << std::endl;
+    std::cout << "The sum of all ints from 50 to 100 inclusive is " << suma << std::endl;
     return 0;
 
 }
