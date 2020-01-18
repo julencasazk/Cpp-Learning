803c9c5999b7f50401a3da56f0f164be3693b7cf
julencasazk <julencasazk@gmail.com>
2020-01-18 13:26:09 +0100

    none

--- while.cpp
+++ while.cpp
@@ -2,12 +2,16 @@
 
 int main()
 {
-    int suma = 0,  val = 50;
-    while (val <= 100) {
-        suma += val; // Asign suma as suma + val
-        ++val; //  Adds 1 to val
+    std::cout << "Introduce two numbers to count between: " << std::endl;
+    int val1 = 0, val2 = 0, sum = 0;
+    std::cin >> val1 >> val2;
+    std::cout << "Counting..." << std::endl;
+    while (val1 <= val2)
+    {
+        sum += val1;
+        ++val1;
     }
-    std::cout << "The sum of all ints from 50 to 100 inclusive is " << suma << std::endl;
-    return 0;
+    std::cout << "The result of the sum is " << sum << std::endl;
+
 
 }
