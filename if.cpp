6fb550278208fc9cc3c3c6df3af42b996f95b3ce
julencasazk <julencasazk@gmail.com>
2020-02-01 18:26:16 +0100

    new if exercices

--- if.cpp
+++ if.cpp
@@ -2,6 +2,29 @@
 
 int main() {
     
-    
+    int current_value = 0, value = 0;
 
+    if (std::cin >> current_value)
+    {
+        int count = 1;
+        while (std::cin >> value)
+        {
+            if (current_value == value)
+            {
+                ++count;
+            }
+            else
+            {
+                std::cout << current_value << " occurs " << count << " times "
+                << std::endl;
+                current_value = value;
+                count = 1;
+
+            }
+            
+        }
+        std::cout << current_value << " occurs " << count << " times "
+                << std::endl;
+    }
+    return 0;
 }
--- while.cpp
+++ while.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+    // Sums all integers between two user inputs
+
+    /*
+    
     std::cout << "Introduce two numbers to count between: " << std::endl;
     int val1 = 0, val2 = 0, sum = 0;
     std::cin >> val1 >> val2;
@@ -13,6 +17,30 @@ int main()
     }
     std::cout << "The result of the sum is " << sum << std::endl;
     return 0;
+
+    */
+
+    // Reads an unknown ammount of user inputs
+
+    /*
+
+    std::cout << "Introduce all integers sequentially or divided by spaces"
+                << std::endl;
+    int total_sum = 0, value = 0;
+    while (std::cin >> value)
+    {
+        total_sum += value;
+        
+    }
+    std::cout << "The sum of all user inputs is: " << total_sum << std::endl;
+    return 0;
+
+    */
+
+    
+    
+    
+
     
 
 
