a89d42bdf864e3118acfc38d474074d14a38bc99
julencasazk <julencasazk@gmail.com>
2020-02-01 17:38:12 +0100

    added new exercise

--- for.cpp
+++ for.cpp
@@ -4,6 +4,10 @@
 
 int main()
 {
+    /// _Sum of all integers between two numbers from user input_
+    
+    /*
+
     std::cout << "Introduce two numbers to count between ( Last inclusive ): " << std::endl;
     int low = 0; int high = 0;
     std::cin >> low >> high;
@@ -16,7 +20,21 @@ int main()
     }
     std::cout << "The sum of all the integers between " << low << " and " << high << " is: " << res << std::endl;
 
-    
+    */
+
+    /// Print numbers from two user inputs downwards
+
+    std::cout << "Introduce two whole numbers to count down from (Higher first): " << std::endl;
+    int low = 0, high = 0;
+    std::cin >> high >> low;
+    for (int i = high; i >= low; --i)
+    {
+        std::cout << i << std::endl;
+    }
+    std::cout << "Done!" << std::endl;
+
+
+
 
 
 }
