f0c477b01512211968da5da3ef8836b6f88bf665
julencasazk <julencasazk@gmail.com>
2020-02-07 13:00:10 +0100

    Updated test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main()
 {
 
     // A test of how a while loop reads user input numbers
-    std::cout << "Introduce a series of numbers: " << std::endl;
+    std::cout << "Introduce a series of numbers:" << std::endl;
     int var = 0;
     while (std::cin >> var) { // It reads a number for each cycle ??
         std::cout << var << std::endl;
