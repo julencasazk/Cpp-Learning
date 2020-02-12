400898cdaca4c749c67b9cbe429aa7f519777ced
julencasazk <julencasazk@gmail.com>
2020-02-12 16:18:39 +0100

    Added new excersice with loops

--- while.cpp
+++ while.cpp
@@ -20,6 +20,36 @@ int main()
 
     */
 
+    // Sum all integers between two user inputs, can put lower first
+
+    std::cout << "Introduce two numbers to count between" << std::endl;
+    int val1 = 0, val2 = 0, sum = 0;
+    std::cin >> val1 >> val2;
+    std::cout << "Counting" << std::endl;
+    if (val1 < val2) {
+        while (val1 <= val2) {
+            sum += val1;
+            ++val1;
+        }
+        std::cout << "The result of the sum is: " << sum << std::endl;
+
+    }
+    else if (val2 < val1 ) {
+        while (val2 <= val1){
+            sum += val2;
+            ++val2;
+        }
+        std::cout << "The result of the sum is: " << sum << std::endl;
+    }
+    else
+    {
+        std::cout << "You introduced two equal numbers!" << std::endl;
+        
+    }
+    
+    
+
+
     // Reads an unknown ammount of user inputs
 
     /*
